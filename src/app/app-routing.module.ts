2ac6991b665272898bff1af78ac51cf78b32f38c
leobenson <leobenson69@gmail.com>
2023-02-27 10:17:06 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:HomeComponent
+  },
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'about',component:AboutComponent
+  },
+  {
+    path:'contact',component:ContactComponent
+  },
+  {
+    path:'login',component:LoginComponent
+  },
+  {
+    path:'services',component:ServicesComponent
+  },
+  {
+    path:'signup',component:SignupComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { NavbarComponent } from './util/navbar/navbar.component';
+import { FooterComponent } from './util/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    SignupComponent,
+    ContactComponent,
+    ServicesComponent,
+    NavbarComponent,
+    FooterComponent,
+     ],
   imports: [
     BrowserModule,
     AppRoutingModule
